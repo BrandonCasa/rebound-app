dc94fe7f309380474c9daf3e5de7570f53b176fc
Brandon <brandoncasa8@gmail.com>
2022-03-17 08:47:18 -0400

    Fixed

--- functions/APIs/todos.js
+++ functions/APIs/todos.js
@@ -1,15 +1,24 @@
+const { db } = require("../util/admin");
+
+// Here we are fetching all the todos from the database and forwarding them to the client in a list.
 exports.getAllTodos = (request, response) => {
-  const todos = [
-    {
-      id: "1",
-      title: "greeting",
-      body: "Hello world from sharvin shah",
-    },
-    {
-      id: "2",
-      title: "greeting2",
-      body: "Hello2 world2 from sharvin shah",
-    },
-  ];
-  return response.json(todos);
+  db.collection("todos")
+    .orderBy("createdAt", "desc")
+    .get()
+    .then((data) => {
+      let todos = [];
+      data.forEach((doc) => {
+        todos.push({
+          todoId: doc.id,
+          title: doc.data().title,
+          body: doc.data().body,
+          createdAt: doc.data().createdAt,
+        });
+      });
+      return response.json(todos);
+    })
+    .catch((err) => {
+      console.error(err);
+      return response.status(500).json({ error: err.code });
+    });
 };
