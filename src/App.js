cdce233f11497c37f0c7d548d749dc2b3bf3c5fe
Brandon <brandoncasa8@gmail.com>
2022-03-23 13:22:08 -0400

    removed tests

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import * as React from "react";
 import ReactDOM from "react-dom";
 import { Link } from "react-router-dom";
 import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
