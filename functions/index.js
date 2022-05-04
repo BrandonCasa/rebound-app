e81c2700d472862b8f73174d43488e7463b04c50
Brandon <brandoncasa8@gmail.com>
2022-05-04 14:37:11 -0400

    some checks

--- functions/index.js
+++ functions/index.js
@@ -7,25 +7,40 @@ const admin = require("firebase-admin");
 admin.initializeApp();
 
 exports.addServerMessage = functions.https.onRequest(async (req, res) => {
-  const writeResult = await admin
-    .firestore()
-    .collection("server_chat")
-    .doc(req.body.serverID)
-    .collection("channels")
-    .doc(req.body.channelID)
-    .collection("messages")
-    .add({ message: req.body.message, timestamp: admin.firestore.FieldValue.serverTimestamp() });
-  res.json({ result: `Message with ID: ${writeResult.id} added.` });
+  const serverDoc = await admin.firestore().collection("server_chat").doc(req.body.serverID).get();
+  if (serverDoc.exists) {
+    const channelDoc = await admin.firestore().collection("server_chat").doc(req.body.serverID).collection("channels").doc(req.body.channelID).get();
+    if (channelDoc.exists) {
+      const writeResult = await admin
+        .firestore()
+        .collection("server_chat")
+        .doc(req.body.serverID)
+        .collection("channels")
+        .doc(req.body.channelID)
+        .collection("messages")
+        .add({ message: req.body.message, timestamp: admin.firestore.FieldValue.serverTimestamp() });
+      res.json({ result: `Message with ID: ${writeResult.id} added to server with ID ${req.body.serverID}.` });
+    } else {
+      res.json({ error: `Channel with ID ${req.body.channelID} does not exist in the server with ID ${req.body.serverID}.` });
+    }
+  } else {
+    res.json({ result: `Server with ID ${req.body.serverID} does not exist.` });
+  }
 });
 
 exports.addServerChannel = functions.https.onRequest(async (req, res) => {
-  const writtenChannel = await admin
-    .firestore()
-    .collection("server_chat")
-    .doc(req.body.serverID)
-    .collection("channels")
-    .add({ channelData: { name: req.body.name, description: req.body.description } });
-  res.json({ result: `Channel with ID ${writtenChannel.id} added to server with ID ${req.body.serverID}` });
+  const serverDoc = await admin.firestore().collection("server_chat").doc(req.body.serverID).get();
+  if (serverDoc.exists) {
+    const writtenChannel = await admin
+      .firestore()
+      .collection("server_chat")
+      .doc(req.body.serverID)
+      .collection("channels")
+      .add({ channelData: { name: req.body.name, description: req.body.description } });
+    res.json({ result: `Channel with ID ${writtenChannel.id} added to server with ID ${req.body.serverID}` });
+  } else {
+    res.json({ result: `Server with ID ${req.body.serverID} does not exist.` });
+  }
 });
 
 exports.addServer = functions.https.onRequest(async (req, res) => {
