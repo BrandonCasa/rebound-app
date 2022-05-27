627612dbe9239cfe64af2a75366ba1a32eec9294
Brandon <brandoncasa8@gmail.com>
2022-05-26 21:48:28 -0400

    new profile page

--- src/pages/Profile.page.tsx
+++ src/pages/Profile.page.tsx
@@ -3,23 +3,48 @@ import * as React from "react";
 import { useParams } from "react-router-dom";
 import { auth } from "../server/index";
 
+function ProfileEditComponent(props: any) {
+  const params = useParams();
+  return (
+    <div style={{ width: "100%", height: "100%" }}>
+      <Grid container spacing={2} justifyContent="center">
+        <Grid item md={6} xs={12}>
+          <Avatar sx={{ width: "48px", height: "48px" }} src={auth.currentUser?.photoURL ? auth.currentUser?.photoURL : undefined} />
+        </Grid>
+        <Grid item md={6} xs={12}></Grid>
+      </Grid>
+    </div>
+  );
+}
+
 function ProfilePage(props: any) {
   const params = useParams();
 
   return (
     <div style={{ display: "flex", flexDirection: "column", flexGrow: 1, marginLeft: "61px", padding: "16px 16px 0px 16px" }}>
       <div style={{ width: "100%", height: "100%" }}>
-        <Grid container spacing={2} justifyContent="center" sx={{ height: "100%" }}>
+        <Grid container spacing={2} justifyContent="center">
           <Grid item md={9} xs={12}>
-            <Paper>lol</Paper>
+            <Paper style={{ width: "100%", height: "100%", margin: "auto", padding: "16px" }}>
+              <Typography textAlign={"center"} variant="h5" fontWeight={400} sx={{ height: "32px", marginBottom: "8px", marginTop: "-8px" }}>
+                {params.userId === auth.currentUser?.uid && "Your Profile"}
+                {
+                  // If not, get their username
+                }
+              </Typography>
+              {params.userId === auth.currentUser?.uid && <ProfileEditComponent />}
+            </Paper>
           </Grid>
           <Grid item md={3} xs={12}>
-            <Paper sx={{ height: "100%", justifyContent: "center" }}>
-              <Card sx={{ maxWidth: 300, maxHeight: 400, margin: "auto" }}>
+            <Paper style={{ width: "100%", height: "100%", margin: "auto", padding: "16px" }}>
+              <Typography textAlign={"center"} variant="h5" fontWeight={400} sx={{ height: "32px", marginBottom: "8px", marginTop: "-8px" }}>
+                Preview
+              </Typography>
+              <Card sx={{ width: 300, margin: "auto" }} variant="outlined">
                 <CardMedia component="img" alt="profile banner" height="172" image={process.env.PUBLIC_URL + "/images/prof1.gif"} />
-                <CardContent sx={{ paddingBottom: "0px", marginBottom: "16px" }}>
+                <CardContent sx={{}}>
                   <div style={{ display: "flex", flexDirection: "row", marginBottom: "16px" }}>
-                    <Avatar src={auth.currentUser ? (auth.currentUser.photoURL ? auth.currentUser.photoURL : undefined) : undefined} />
+                    <Avatar src={auth.currentUser?.photoURL ? auth.currentUser?.photoURL : undefined} />
                     <Typography gutterBottom variant="h5" component="div" sx={{ margin: "auto 0 auto 16px" }}>
                       {auth.currentUser && auth.currentUser.displayName}
                     </Typography>
